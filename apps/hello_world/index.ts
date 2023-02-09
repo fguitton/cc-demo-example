f98e802d1f55baad174cbda993d3629bbc58e6d4
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2023-02-09 13:55:36 +0000

    Update index.ts

--- apps/hello_world/index.ts
+++ apps/hello_world/index.ts
@@ -1,16 +1,17 @@
-import * as Klave from '@secretarium/trustless-app';
-
-export const my_query: Klave.Query = (arg) => {
-    const s = String.UTF8.decode(arg, true);
-    Klave.notify(String.UTF8.encode('Hello ' + s, true));
-};
-
-export const my_transaction: Klave.Transaction = (arg) => {
-    const s = String.UTF8.decode(arg, true);
-    Klave.notify(String.UTF8.encode('Yipi ' + s, true));
-};
+@external("env", "notify")
+    declare function notify(s: ArrayBuffer): i32;
+@external("env", "add_user_query")
+    declare function add_user_query(s: ArrayBuffer): void;
 
 export function register_routes(): void {
-    Klave.addUserQuery(String.UTF8.encode('my_query', true));
-    Klave.addUserTransaction(String.UTF8.encode('my_transaction', true));
+    add_user_query(String.UTF8.encode("my_query", true));
+}
+
+export function my_query(arg: ArrayBuffer): void {
+      let arr = new Uint8Array(4);
+      arr[0] = 65;
+      arr[1] = 66;
+      arr[2] = 67;
+      arr[3] = 0;
+      notify(arr.buffer);
 }
